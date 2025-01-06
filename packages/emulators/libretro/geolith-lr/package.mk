283d57368b99a0e91434499aa42c913fa4170c48
alhumbra <alhumbra@gmail.com>
2025-01-06 20:39:41 +0900

    fix
    ビルドエラーが出るのでドキュメントを復旧しました。
    package.mkのURLの修正。

--- packages/emulators/libretro/geolith-lr/package.mk
+++ packages/emulators/libretro/geolith-lr/package.mk
@@ -10,10 +10,12 @@ PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Geolith is a highly accurate emulator for the Neo Geo AES and MVS."
 PKG_TOOLCHAIN="make"
+
 make_target() {
 cd libretro
   make -f ./Makefile platform=${DEVICE_NAME}
 }
+
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/lib/libretro
   cp geolith_libretro.so ${INSTALL}/usr/lib/libretro/
--- packages/virtual/emulators/package.mk
+++ packages/virtual/emulators/package.mk
@@ -55,7 +55,7 @@ case "${DEVICE}" in
     [ "${ENABLE_32BIT}" == "true" ] && EMUS_32BIT="box86 desmume-lr gpsp-lr pcsx_rearmed-lr"
     PKG_DEPENDS_TARGET+=" common-shaders glsl-shaders mupen64plus-sa scummvmsa box64 portmaster"
     PKG_EMUS+=" amiberry drastic-sa yabasanshiro-sa"
-    LIBRETRO_CORES+=" uae4arm flycast-lr geolith-lr"
+    LIBRETRO_CORES+=" geolith-lr uae4arm flycast-lr"
     PKG_RETROARCH+=" retropie-shaders"
   ;;
   S922X*)
